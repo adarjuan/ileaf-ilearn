8a085d40184654b58caff6d1c4e70a74f24d1fab
Alyssa Darjuan <adarjuan@irislocal.com>
2019-02-16 15:08:01 -0800

    started quiz component, added router and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'quiz', component: QuizComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
